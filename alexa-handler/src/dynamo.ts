9c254bc42501b9088fd0bff269df649a70b8769c
Oleg Legun <oleg.legun@gmail.com>
2018-07-10 21:59:02 +0300

    add DynamoDB helper functions

---
+++ alexa-handler/src/dynamo.ts
@@ -0,0 +1,93 @@
+import { DynamoDB } from 'aws-sdk'
+import { DDB_NEWS_KEY, DDB_TABLE_NAME } from './env'
+import { UnsetEnvironmentVariableError } from './errors'
+
+const DDBClient = new DynamoDB.DocumentClient({ apiVersion: '2012-08-10' })
+
+async function getNewsItems(): Promise<INewsItemDDB[]> {
+    return new Promise<INewsItemDDB[]>((resolve, reject) => {
+        if (!DDB_TABLE_NAME || !DDB_NEWS_KEY) {
+            reject(new UnsetEnvironmentVariableError('DDB_TABLE_NAME/DDB_NEWS_KEY'))
+            return
+        }
+
+        const params: DynamoDB.DocumentClient.GetItemInput = {
+            Key: {
+                id: DDB_NEWS_KEY,
+            },
+            TableName: DDB_TABLE_NAME,
+        }
+
+        DDBClient.get(params, (err, data) => {
+            if (err) {
+                reject(err)
+                return
+            }
+
+            if (data.Item) {
+                resolve(data.Item.attributes)
+            } else {
+                reject(
+                    new Error(
+                        `No news items found in table "${DDB_TABLE_NAME}" for key "${DDB_NEWS_KEY}." `
+                    )
+                )
+            }
+        })
+    })
+}
+
+async function getUserAttributes(id: string): Promise<IUserDDB | undefined> {
+    return new Promise<IUserDDB | undefined>((resolve, reject) => {
+        if (!DDB_TABLE_NAME) {
+            reject(new UnsetEnvironmentVariableError('DDB_TABLE_NAME'))
+            return
+        }
+
+        const params: DynamoDB.DocumentClient.GetItemInput = {
+            Key: { id },
+            TableName: DDB_TABLE_NAME,
+        }
+
+        DDBClient.get(params, (err, data) => {
+            if (err) {
+                reject(err)
+                return
+            }
+
+            if (!data.Item) {
+                resolve(undefined)
+                return
+            }
+
+            resolve(data.Item.attributes)
+        })
+    })
+}
+
+async function putUserAttributes(id: string, attributes: IUserDDB) {
+    return new Promise((resolve, reject) => {
+        if (!DDB_TABLE_NAME) {
+            reject(new UnsetEnvironmentVariableError('DDB_TABLE_NAME'))
+            return
+        }
+
+        const params: DynamoDB.DocumentClient.PutItemInput = {
+            Item: {
+                id,
+                attributes,
+            },
+            TableName: DDB_TABLE_NAME,
+        }
+
+        DDBClient.put(params, (err, data) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            resolve()
+        })
+    })
+}
+
+export { getNewsItems, getUserAttributes, putUserAttributes }
