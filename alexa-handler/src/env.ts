646ee147d93ea6db5232ba0194533ef5f10d242f
Oleg Legun <oleg.legun@gmail.com>
2018-07-10 22:07:26 +0300

    extract env vars into separate file

---
+++ alexa-handler/src/env.ts
@@ -0,0 +1,5 @@
+const DDB_TABLE_NAME = process.env['DDB_TABLE_NAME']
+const DDB_NEWS_KEY = process.env['DDB_NEWS_KEY']
+const DEBUG = process.env['DEBUG']
+
+export { DDB_TABLE_NAME, DDB_NEWS_KEY, DEBUG }
