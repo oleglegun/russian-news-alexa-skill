af1a2c884ba1cf0ec808d6bf759af3305d50afdf
Oleg Legun <oleg.legun@gmail.com>
2018-07-11 19:44:13 +0300

    update types

--- alexa-handler/src/types.ts
+++ alexa-handler/src/types.ts
@@ -1,20 +1,26 @@
-interface INewsItemRSS {
-    id: string
-    date: string
-    title: string
-    text: string
-    sourceURL: string
-    imageURL?: string
-}
-
-interface INewsItemRSSWithSSML extends INewsItemRSS {
-    ssml: string
-}
-
 interface INewsItemDDB {
     Id: string
     Title: string
     SourceURL: string
     AudioURL: string
     ImageURL: string
+}
+
+interface IUserDDB {
+    FirstAccess: string
+    LastAccess: string
+    LastPlayedItem?: string
+    DaysActive: number
+    ItemsConsumed: number
+    Devices: string[]
+}
+
+type IGetUserAttributes = (id: string) => Promise<IUserDDB | undefined>
+
+interface IRequestAttributes {
+    getNews(): Promise<INewsItemDDB[]>
+    getNextNewsItem(currentNewsId: string): Promise<INewsItemDDB | undefined>
+    getUser(): Promise<IUserDDB | undefined>
+    putUser(attributes: IUserDDB): Promise<void>
+    createNewUser(): IUserDDB
 }
