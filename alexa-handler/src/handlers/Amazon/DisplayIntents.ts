578b64040d55c979cecee58b20ac5a3cf8b1f557
Oleg Legun <oleg.legun@gmail.com>
2018-07-12 20:15:46 +0300

    add Display intents stub

---
+++ alexa-handler/src/handlers/Amazon/DisplayIntents.ts
@@ -0,0 +1,26 @@
+import * as ASK from 'ask-sdk'
+
+export const DisplayIntentsHandler: ASK.RequestHandler = {
+    canHandle(handlerInput) {
+        const intentNames = [
+            'AMAZON.MoreIntent',
+            'AMAZON.NavigateHomeIntent',
+            'AMAZON.NavigateSettingsIntent',
+            'AMAZON.NextIntent',
+            'AMAZON.PageUpIntent',
+            'AMAZON.PageDownIntent',
+            'AMAZON.PreviousIntent',
+            'AMAZON.ScrollRightIntent',
+            'AMAZON.ScrollDownIntent',
+            'AMAZON.ScrollLeftIntent',
+            'AMAZON.ScrollUpIntent',
+        ]
+        return (
+            handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+            intentNames.indexOf(handlerInput.requestEnvelope.request.intent.name) !== -1
+        )
+    },
+    async handle(handlerInput) {
+        return handlerInput.responseBuilder.getResponse()
+    },
+}
